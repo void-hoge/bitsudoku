a423da56613a32d1f2c33458260b413fcac4d3ca
void-hoge <coilgunrailgun@gmail.com>
2021-08-11 19:04:34 +0900

    command line select option

--- main.cpp
+++ main.cpp
@@ -195,16 +195,27 @@ void test16() {
 }
 
 int main(int argc, char const *argv[]) {
-	if (argc != 2) {
+	if (argc != 3) {
 		throw std::invalid_argument("too much/few arguments.");
 	}
 	std::cout << "bit board sudoku" << std::endl;
 	std::string data;
-	read_text_file(std::string(argv[1]), data);
-	sudoku::board<4> bd;
-	bd.string_input(data);
-	bd.show();
-	sudoku::solver<4> slv;
-	slv.solve(bd);
+	read_text_file(std::string(argv[2]), data);
+	int opt = std::stoi(argv[1]);
+	if (opt == 3) {
+		sudoku::board<3> bd;
+		bd.string_input(data);
+		bd.show();
+		sudoku::solver<3> slv;
+		slv.solve(bd);
+	} else if (opt == 4) {
+		sudoku::board<4> bd;
+		bd.string_input(data);
+		bd.show();
+		sudoku::solver<4> slv;
+		slv.solve(bd);
+	} else {
+		throw std::invalid_argument("bitsudoku [3|4] *inputfile.txt*");
+	}
 	return 0;
 }
